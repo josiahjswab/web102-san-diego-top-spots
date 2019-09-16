13f260596ff7ac2ea5768f99bf5b9aaa9c3bd445
Josiah Swab <josiahjswab@gmail.com>
2019-09-16 14:17:30 -0700

    css added working on api for google maps

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
-$(document).ready(function() {
-// write your code here
-
+$(document).ready(function (data) {
+  $.getJSON("data.json", function (data) {
+    $.each(data, function (index, value) {
+        $("table").append("<tr><td>" 
+        + value.name + "</td><td>" 
+        + value.description + "</td><td><a href=https://www.google.com/maps?q=" 
+        + value.location +  " target='_blank'><button id='google-button'>Google Maps</button></a></td></tr>")
+    });
+  }); 
 });
